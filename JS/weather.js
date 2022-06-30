7cd4496eaec336182527a11e364e34219c1c5fac
safa ayachi <safa.ayachi@polytechnicien.tn>
2022-06-30 19:05:33 +0100

    location bt

--- JS/weather.js
+++ JS/weather.js
@@ -50,12 +50,12 @@ const fifthDayMinTempElement = document.getElementById(
   "fifthDayMin-temperature"
 );
 const API_KEY = "3a61d03d191e00e1fa908edb1ff5d13c";
-locationElement.addEventListener("click",getWeatherData());
+getWeatherData();
 function getWeatherData() {
   navigator.geolocation.getCurrentPosition((success) => {
     console.log(success);
     let {latitude, longitude}=success.coords;
-    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&appid=${API_KEY}`).then(res => res.json()).then(data=>{
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`).then(res => res.json()).then(data=>{
         console.log(data);
     })
   });
