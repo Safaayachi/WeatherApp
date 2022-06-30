02ae4c47223f184092a3941f72b4cb39ada973a8
safa ayachi <safa.ayachi@polytechnicien.tn>
2022-06-30 23:38:41 +0100

    js

--- JS/weather.js
+++ JS/weather.js
@@ -36,7 +36,6 @@ const API_KEY = "3a61d03d191e00e1fa908edb1ff5d13c";
     let { latitude, longitude } = success.coords;
     fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`).then(res => res.json()).then(data =>{
     console.log(data)
-    showWeatherData(data);
     })
   })
 }
